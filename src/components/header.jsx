c51074b9dbdb5b5a85eef027bbcabbf4ef055fbf
Zepthot <zepthot@gmail.com>
2023-08-17 11:16:01 +0200

    Rework header; Add download file on CV button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,14 @@
 import '../styles/header.scss';
 import logo from '../assets/logo.webp';
 import flagfr from '../assets/flag-fr.webp';
+import flagen from '../assets/flag-fr.webp';
 
 // Header function for page header
 function Header() {
+    function handleClick() {
+        console.log("Language button");
+      };
+
     return (
         <header className='header-component'>
             {/* Logo home */}
@@ -15,11 +20,13 @@ function Header() {
                     <li><a href='#portfolio'>Portfolio</a></li>
                     <li><a href='#skills'>Compétences</a></li>
                     <li><a href='#contact'>Contact</a></li>
-                    <li><a href='#cv'>CV</a></li>
+                    <li><a href='./CV.pdf' download>CV</a></li>
                 </ul>
             </nav>
-            {/* Language logo */}
-            <img src={flagfr} alt='Drapeau français' className='flags'/>
+            {/* Language button */}
+            <button className='button-flags' onClick={handleClick}>
+                <img src={flagfr} alt='Drapeau français' className='flags'/>
+            </button>
         </header>
     )
 }
